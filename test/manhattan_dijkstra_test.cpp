c800481fdf38152573de427030c62e6c5d6d5ef8
lukas <np-hardass@protonmail.com>
2020-08-29 23:40:19 +0200

    added test cases for the manhattan dijkstra

---
+++ test/manhattan_dijkstra_test.cpp
@@ -0,0 +1,63 @@
+#include <GridGraph.hpp>
+#include <ManhattanDijkstra.hpp>
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+
+#include <gtest/gtest.h>
+
+using graph::Node;
+using graph::GridGraph;
+using pathfinding::ManhattanDijkstra;
+using pathfinding::Path;
+
+
+TEST(ManhattanDijkstraTest, ManhattanDijkstraWithoutBarriersTest)
+{
+    std::vector test1{
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true}};
+
+    GridGraph graph_test1{test1};
+
+    ASSERT_EQ(graph_test1.width, 5);
+    ASSERT_EQ(graph_test1.height, 5);
+
+    ManhattanDijkstra d{graph_test1};
+
+    auto paths = d.findRoutes({0, 0}, {0, 4});
+
+    for(auto& p : paths) {
+        EXPECT_EQ(p.getLength(), 4);
+    }
+
+    EXPECT_EQ(paths.size(), 1);
+}
+
+TEST(ManhattanDijkstraTest, ManhattanDijkstraWithBarrierTest)
+{
+    std::vector test1{
+        std::vector{true, true, false, true, true},
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true},
+        std::vector{true, true, true, true, true}};
+
+    GridGraph graph_test1{test1};
+
+    ASSERT_EQ(graph_test1.width, 5);
+    ASSERT_EQ(graph_test1.height, 5);
+
+    ManhattanDijkstra d{graph_test1};
+
+    auto paths = d.findRoutes({0, 0}, {0, 4});
+
+    for(auto& p : paths) {
+        EXPECT_EQ(p.getLength(), 6);
+    }
+
+    EXPECT_EQ(paths.size(), 4);
+}
