55aec2244c7488e51ef3efcba70bebc1f77ce37a
lukas <np-hardass@protonmail.com>
2020-09-01 01:16:20 +0200

    use universal forwarding for the min and max function

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 namespace util {
 
 template<class Head0, class Head1, class... Tail>
@@ -8,7 +10,10 @@ constexpr auto min(Head0&& head0, Head1&& head1, Tail&&... tail)
     if constexpr(sizeof...(tail) == 0) {
         return head0 < head1 ? head0 : head1;
     } else {
-        return min(min(head0, head1), tail...);
+        return min(
+            min(std::forward<Head0>(head0),
+                std::forward<Head1>(head1)),
+            std::forward<Tail>(tail)...);
     }
 }
 
@@ -18,7 +23,10 @@ constexpr auto max(Head0&& head0, Head1&& head1, Tail&&... tail)
     if constexpr(sizeof...(tail) == 0) {
         return head0 > head1 ? head0 : head1;
     } else {
-        return min(min(head0, head1), tail...);
+        return max(
+            max(std::forward<Head0>(head0),
+                std::forward<Head1>(head1)),
+            std::forward<Tail>(tail)...);
     }
 }
 
