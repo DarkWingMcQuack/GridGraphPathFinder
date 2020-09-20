077a7a97e3c9be2868f869984919c6d33e77be95
lukas <np-hardass@protonmail.com>
2020-09-20 17:25:33 +0200

    added the headers for the Column and Row walkers

---
+++ include/graph/CellColumnIterator.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <array>
+#include <graph/GridCellIterator.hpp>
+#include <graph/GridCorner.hpp>
+#include <graph/Node.hpp>
+#include <iostream>
+
+namespace graph {
+
+class CellColumnIterator
+{
+public:
+    using difference_type = long;
+    using value_type = GridCell;
+    using pointer = GridCell;
+    using reference = GridCell;
+    using iterator_category = std::random_access_iterator_tag;
+
+    explicit CellColumnIterator(const GridCell& cell,
+                                difference_type idx = 0);
+
+    auto operator++() noexcept
+        -> CellColumnIterator&;
+
+    auto operator++(int) noexcept
+        -> CellColumnIterator;
+
+    auto operator--() noexcept
+        -> CellColumnIterator&;
+
+    auto operator--(int) noexcept
+        -> CellColumnIterator;
+
+    auto operator+(difference_type rhs) const noexcept
+        -> CellColumnIterator;
+    auto operator-(difference_type rhs) const noexcept
+        -> CellColumnIterator;
+
+    auto operator+(const CellColumnIterator& rhs) const noexcept
+        -> difference_type;
+    auto operator-(const CellColumnIterator& rhs) const noexcept
+        -> difference_type;
+
+    auto operator+=(difference_type rhs) noexcept
+        -> CellColumnIterator&;
+    auto operator-=(difference_type rhs) noexcept
+        -> CellColumnIterator&;
+    auto operator+=(const CellColumnIterator& rhs) noexcept
+        -> CellColumnIterator&;
+    auto operator-=(const CellColumnIterator& rhs) noexcept
+        -> CellColumnIterator&;
+
+    auto operator==(const CellColumnIterator& other) const noexcept
+        -> bool;
+    auto operator!=(const CellColumnIterator& other) const noexcept
+        -> bool;
+    auto operator<=(const CellColumnIterator& other) const noexcept
+        -> bool;
+    auto operator>=(const CellColumnIterator& other) const noexcept
+        -> bool;
+    auto operator<(const CellColumnIterator& other) const noexcept
+        -> bool;
+    auto operator>(const CellColumnIterator& other) const noexcept
+        -> bool;
+
+    auto operator*() noexcept -> GridCell;
+    auto operator->() noexcept -> GridCell;
+    auto operator[](int) noexcept -> GridCell;
+
+
+private:
+    const GridCell& cell_;
+    difference_type idx_;
+};
+
+class CellColumnWalker
+{
+public:
+    CellColumnWalker(const GridCell& cell);
+
+
+    [[nodiscard]] auto begin() const noexcept
+        -> CellColumnIterator;
+
+    [[nodiscard]] auto end() const noexcept
+        -> CellColumnIterator;
+
+private:
+    const GridCell& cell;
+};
+
+} // namespace graph
---
+++ include/graph/CellRowWalker.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <array>
+#include <graph/GridCellIterator.hpp>
+#include <graph/GridCorner.hpp>
+#include <graph/Node.hpp>
+#include <iostream>
+
+namespace graph {
+
+class CellRowIterator
+{
+public:
+    using difference_type = long;
+    using value_type = GridCell;
+    using pointer = GridCell;
+    using reference = GridCell;
+    using iterator_category = std::random_access_iterator_tag;
+
+    explicit CellRowIterator(const GridCell& cell,
+                             difference_type idx = 0);
+
+    auto operator++() noexcept
+        -> CellRowIterator&;
+
+    auto operator++(int) noexcept
+        -> CellRowIterator;
+
+    auto operator--() noexcept
+        -> CellRowIterator&;
+
+    auto operator--(int) noexcept
+        -> CellRowIterator;
+
+    auto operator+(difference_type rhs) const noexcept
+        -> CellRowIterator;
+    auto operator-(difference_type rhs) const noexcept
+        -> CellRowIterator;
+
+    auto operator+(const CellRowIterator& rhs) const noexcept
+        -> difference_type;
+    auto operator-(const CellRowIterator& rhs) const noexcept
+        -> difference_type;
+
+    auto operator+=(difference_type rhs) noexcept
+        -> CellRowIterator&;
+    auto operator-=(difference_type rhs) noexcept
+        -> CellRowIterator&;
+    auto operator+=(const CellRowIterator& rhs) noexcept
+        -> CellRowIterator&;
+    auto operator-=(const CellRowIterator& rhs) noexcept
+        -> CellRowIterator&;
+
+    auto operator==(const CellRowIterator& other) const noexcept
+        -> bool;
+    auto operator!=(const CellRowIterator& other) const noexcept
+        -> bool;
+    auto operator<=(const CellRowIterator& other) const noexcept
+        -> bool;
+    auto operator>=(const CellRowIterator& other) const noexcept
+        -> bool;
+    auto operator<(const CellRowIterator& other) const noexcept
+        -> bool;
+    auto operator>(const CellRowIterator& other) const noexcept
+        -> bool;
+
+    auto operator*() noexcept -> GridCell;
+    auto operator->() noexcept -> GridCell;
+    auto operator[](int) noexcept -> GridCell;
+
+
+private:
+    const GridCell& cell_;
+    difference_type idx_;
+};
+
+class CellRowWalker
+{
+public:
+    CellRowWalker(const GridCell& cell);
+
+
+    [[nodiscard]] auto begin() const noexcept
+        -> CellRowIterator;
+
+    [[nodiscard]] auto end() const noexcept
+        -> CellRowIterator;
+
+private:
+    const GridCell& cell;
+};
+
+} // namespace graph
