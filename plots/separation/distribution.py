cb0d5c7fb534a93f37965a96e4e7d2cc869f0a9b
lukas <np-hardass@protonmail.com>
2020-10-29 16:43:34 +0100

    added a script to create bar plots from the separation distribution data

---
+++ plots/separation/distribution.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from mayavi import mlab
+from mpl_toolkits.mplot3d import Axes3D
+import csv
+
+def read_csv(path):
+    with open(path,'r') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        tuples = [(int(row[0]), int(row[1]), int(row[2])) for row in data]
+        x = [x for x, _, _ in tuples]
+        y = [y for _, y, _ in tuples]
+        z = [z for _, _, z in tuples]
+        return x, y, z
+
+
+def plot3D(x, y, z):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    surf = ax.plot_trisurf(x, y, z, cmap=cm.jet, linewidth=0.1)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
+def plotBar(x, y, z):
+    xy = [f"{x}:{y}" for x, y in zip(x, y)]
+
+    plt.bar(xy, z)
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='distribution plot')
+    parser.add_argument('--file', '-f', required=True, type=str)
+    args = parser.parse_args()
+
+    path = args.file
+    x, y, z = read_csv(path)
+
+    plotBar(x, y, z)
