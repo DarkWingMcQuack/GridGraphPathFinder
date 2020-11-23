c9895f22473725190135f1a0c8a88698f3c57ab4
lukas <np-hardass@protonmail.com>
2020-11-23 17:31:37 +0100

    added a header file for selection buckets

---
+++ include/selection/SelectionBucket.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <graph/Node.hpp>
+#include <selection/NodeSelection.hpp>
+#include <vector>
+
+namespace selection {
+
+class SelectionBucket
+{
+    SelectionBucket(std::vector<NodeSelection> selections);
+
+    [[nodiscard]] auto weight() const noexcept
+        -> std::size_t;
+
+    [[nodiscard]] auto isSubSetOf(const SelectionBucket& other) const noexcept
+        -> bool;
+
+    [[nodiscard]] auto isSuperSetOf(const SelectionBucket& other) const noexcept
+        -> bool;
+
+    [[nodiscard]] auto contains(const NodeSelection& other) const noexcept
+        -> bool;
+
+    [[nodiscard]] auto containsAll(const std::vector<NodeSelection>& other) const noexcept
+        -> bool;
+
+    [[nodiscard]] auto canAnswer(const graph::Node& from,
+                                 const graph::Node& to) const noexcept
+        -> bool;
+
+    [[nodiscard]] auto merge(SelectionBucket other) const noexcept
+        -> SelectionBucket;
+
+    [[nodiscard]] auto getSelections() const noexcept
+        -> const std::vector<NodeSelection>&;
+
+private:
+    std::vector<NodeSelection> selections_;
+};
+
+} // namespace selection
