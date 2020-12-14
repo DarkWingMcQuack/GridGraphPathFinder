301e7fa61d3dd0c0eb8f728fd1fe605ac39e8d54
lukas <np-hardass@protonmail.com>
2020-12-14 12:16:23 +0100

    added a little test for the zscore

---
+++ test/node_test.cpp
@@ -0,0 +1,21 @@
+#include <fmt/core.h>
+#include <graph/Node.hpp>
+
+#include <gtest/gtest.h>
+
+using graph::Node;
+
+TEST(GridGraphTest, NodeZScoreTest)
+{
+    Node first{0, 0};
+    Node second{1, 0};
+
+    EXPECT_TRUE(first < second);
+    EXPECT_FALSE(second < first);
+
+    first = {1, 5};
+    second = {0, 1};
+    fmt::print("z: {}\n", first.zScore());
+    EXPECT_TRUE(first < second);
+    EXPECT_FALSE(second < first);
+}
