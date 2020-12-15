b9793563a7dbf16f2ad4553d2f8058fc09250170
lukas <np-hardass@protonmail.com>
2020-12-15 18:08:58 +0100

    the bucket lookup now works with indices instead of references

--- include/selection/SelectionBucketLookup.hpp
+++ include/selection/SelectionBucketLookup.hpp
@@ -15,12 +15,12 @@ class SelectionBucketLookup
 public:
     SelectionBucketLookup(const graph::GridGraph& graph,
                           std::vector<SelectionBucket> buckets,
-                          std::vector<utils::RefVec<SelectionBucket>> bucket_lookup);
+                          std::vector<std::vector<std::size_t>> bucket_lookup);
 
 private:
     const graph::GridGraph& graph_;
     std::vector<SelectionBucket> buckets_;
-    std::vector<utils::RefVec<SelectionBucket>> bucket_lookup_;
+    std::vector<std::vector<std::size_t>> bucket_lookup_;
 };
 
 } // namespace selection
--- src/selection/SelectionBucketLookup.cpp
+++ src/selection/SelectionBucketLookup.cpp
@@ -13,7 +13,7 @@ using selection::SelectionBucketLookup;
 
 SelectionBucketLookup::SelectionBucketLookup(const graph::GridGraph& graph,
                                              std::vector<SelectionBucket> buckets,
-                                             std::vector<utils::RefVec<SelectionBucket>> bucket_lookup)
+                                             std::vector<std::vector<std::size_t>> bucket_lookup)
     : graph_(graph),
       buckets_(std::move(buckets)),
       bucket_lookup_(std::move(bucket_lookup)) {}
