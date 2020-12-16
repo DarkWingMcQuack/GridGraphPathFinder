cc2d5703303fa3bdc248a0fe95b224f631c77ee5
lukas <np-hardass@protonmail.com>
2020-12-16 17:49:20 +0100

    forgot to push the header

--- include/pathfinding/Dijkstra.hpp
+++ include/pathfinding/Dijkstra.hpp
@@ -27,16 +27,16 @@ public:
     auto operator=(const Dijkstra&) -> Dijkstra& = delete;
     auto operator=(Dijkstra&&) -> Dijkstra& = delete;
 
-    [[nodiscard]] auto findAllRoutes(const graph::Node& source, const graph::Node& target) noexcept
+    [[nodiscard]] auto findAllRoutes(graph::Node source, graph::Node target) noexcept
         -> std::vector<Path>;
 
-    [[nodiscard]] auto findRoute(const graph::Node& source, const graph::Node& target) noexcept
+    [[nodiscard]] auto findRoute(graph::Node source, graph::Node target) noexcept
         -> std::optional<Path>;
 
-    [[nodiscard]] auto findDistance(const graph::Node& source, const graph::Node& target) noexcept
+    [[nodiscard]] auto findDistance(graph::Node source, graph::Node target) noexcept
         -> graph::Distance;
 
-    [[nodiscard]] auto findTrivialDistance(const graph::Node& source, const graph::Node& target) noexcept
+    [[nodiscard]] auto findTrivialDistance(graph::Node source, graph::Node target) noexcept
         -> graph::Distance;
 
     [[nodiscard]] auto getNodesWithMinDistanceIn(const graph::GridCell& cell) noexcept
@@ -49,37 +49,40 @@ public:
         -> const graph::GridGraph&;
 
 protected:
-    [[nodiscard]] auto getDistanceTo(const graph::Node& n) const noexcept
+    [[nodiscard]] auto getDistanceTo(graph::Node n) const noexcept
         -> graph::Distance;
 
-    auto setDistanceTo(const graph::Node& n, std::int64_t distance) noexcept
+    auto setDistanceTo(graph::Node n, std::int64_t distance) noexcept
         -> void;
 
-    [[nodiscard]] auto computeDistance(const graph::Node& source, const graph::Node& target) noexcept
+    [[nodiscard]] auto computeDistance(graph::Node source, graph::Node target) noexcept
         -> graph::Distance;
 
-    [[nodiscard]] auto extractAllShortestPaths(const graph::Node& source, const graph::Node& target) const noexcept
+    [[nodiscard]] auto extractAllShortestPaths(graph::Node source, graph::Node target) const noexcept
         -> std::vector<Path>;
 
-    [[nodiscard]] auto extractShortestPath(const graph::Node& source, const graph::Node& target) const noexcept
+    [[nodiscard]] auto extractShortestPath(graph::Node source, graph::Node target) const noexcept
         -> std::optional<Path>;
 
     [[nodiscard]] auto findSmallestDistance(const std::vector<graph::Node>& nodes) const noexcept
         -> graph::Distance;
 
-    [[nodiscard]] auto getIndex(const graph::Node& n) const noexcept
+    [[nodiscard]] auto getIndex(graph::Node n) const noexcept
         -> std::optional<std::size_t>;
 
-    auto unSettle(const graph::Node& n)
+    auto unSettle(graph::Node n)
         -> void;
 
-    auto settle(const graph::Node& n) noexcept
+    auto settle(graph::Node n) noexcept
         -> void;
 
-    [[nodiscard]] auto isSettled(const graph::Node& n)
+    [[nodiscard]] auto isSettled(graph::Node n)
         -> bool;
 
     auto reset() noexcept
+        -> void;
+
+    auto setBefore(graph::Node n, graph::Node before) noexcept
         -> void;
 
 private:
@@ -89,6 +92,7 @@ private:
     std::vector<graph::Node> touched_;
     DijkstraQueue pq_;
     std::optional<graph::Node> last_source_;
+    std::vector<graph::Node> before_;
 };
 
 
